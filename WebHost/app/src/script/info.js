74ecf7cb75df4c28d2b4f7bd51899602dbbd5dcb
yeonyeonn <wuvele22@g.skku.edu>
2023-12-05 05:43:46 +0900

    update info page

--- WebHost/app/src/script/info.js
+++ WebHost/app/src/script/info.js
@@ -4,20 +4,22 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-import info from '../data/info.json'; // info.json 파일을 올바른 경로로 import
+import info from '../data/info.json'; 
+import img from "../img/psj.jpeg"
+import "../style/info.css";
 
 const LoungeCards = () => {
   return (
     <div>
       {info.map((lounge) => (
-        <Card key={lounge.key} sx={{ maxWidth: 345, marginBottom: 2 }}>
+        <Card key={lounge.key} sx={{ maxWidth: 400, height: 400, marginBottom: 10 }}>
           <CardActionArea>
             <CardMedia
               component="img"
-              height="140"
-              // 예시 이미지 경로를 사용하거나, lounge 객체에 이미지 경로를 추가할 수 있습니다.
-              image="/static/images/cards/contemplative-reptile.jpg"
-              alt={lounge.name}
+              alt="SKKU Image"
+              image={img}
+              className="skku-image"
+              sx={{ height: 'auto' }} 
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
