45f037c9173987ec9216bfd587ac8a28f4ce0251
yeonyeonn <wuvele22@g.skku.edu>
2023-12-05 07:10:47 +0900

    update info page

--- WebHost/app/src/script/info.js
+++ WebHost/app/src/script/info.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -7,19 +7,43 @@ import { CardActionArea } from '@mui/material';
 import info from '../data/info.json'; 
 import "../style/info.css";
 
+// const handleTakeSeat = ({ spaceName }) => {
+//   navigate({
+//     pathname: "/lounge",
+//     search: createSearchParams({ spaceName: spaceName }).toString()
+//   });
+// };
+
+
+// // ActionButton Component
+// const ActionButton = ({ onTakeSeat, className }) => (
+//     <button className={`take-seat-button ${className}`} onClick={onTakeSeat}>
+//       Take a seat
+//     </button>
+//   );  
+
 const LoungeCards = () => {
+  const [selectedCard, setSelectedCard] = useState(null);
+
+  const handleCardClick = (key) => {
+    setSelectedCard(key === selectedCard ? null : key);
+  };
 
   return (
     <div className='container'>
       {info.map((lounge) => (
-        // 카드 height 300으로 고정
-        <Card key={lounge.key} sx={{ maxWidth: 400, height: 300 }} className='card'> 
+        <Card 
+          key={lounge.key} 
+          sx={{ maxWidth: 400, height: 300, position: 'relative' }} 
+          className='card'
+          onClick={() => handleCardClick(lounge.key)}
+        >
           <CardActionArea>
             <CardMedia
               component="img"
               alt={lounge.name}
               image={`/img/${lounge.img}`}
-              sx={{ height: 200, objectFit: 'cover' }} // 사진 크기 200으로 고정
+              sx={{ height: 200, objectFit: 'cover' }}
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
@@ -29,7 +53,11 @@ const LoungeCards = () => {
                 {lounge.description}
               </Typography>
             </CardContent>
+            
           </CardActionArea>
+          {selectedCard === lounge.key && <div className="card-overlay">
+          {/* <ActionButton onTakeSeat={onTakeSeat} /> */}
+            </div>}
         </Card>
       ))}
     </div>
