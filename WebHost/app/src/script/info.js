5ba450402fc4e01429ea7878f771be1747a9070f
yeonyeonn <wuvele22@g.skku.edu>
2023-12-05 04:56:02 +0900

    update info page

--- WebHost/app/src/script/info.js
+++ WebHost/app/src/script/info.js
@@ -1,30 +1,37 @@
-import * as React from 'react';
+import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import info from '../data/info.json'; // info.json 파일을 올바른 경로로 import
 
-export default function ActionAreaCard() {
+const LoungeCards = () => {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <div>
+      {info.map((lounge) => (
+        <Card key={lounge.key} sx={{ maxWidth: 345, marginBottom: 2 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              // 예시 이미지 경로를 사용하거나, lounge 객체에 이미지 경로를 추가할 수 있습니다.
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt={lounge.name}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {lounge.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {lounge.description}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      ))}
+    </div>
   );
-}
+};
+
+export default LoungeCards;
