b99f1635d2689fe9afc64d2150af64eb8db0b264
j2yp <jiwoo0110@gmail.com>
2023-12-05 03:19:40 +0900

    Update App.js

--- WebHost/app/src/App.js
+++ WebHost/app/src/App.js
@@ -3,14 +3,15 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Intro from "./script/intro.js";
 import Lounges from "./script/lounges.js";
 import Lounge from "./script/lounge.js";
+import Nav from "./script/nav.js"
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Intro />} />
-        <Route path="/lounges" element={<Lounges />} />
-        <Route path="/lounge" element={<Lounge />} />
+        <Route path="/lounges" element={<><Lounges /> <Nav/></>} /> 
+        <Route path="/lounge" element={<><Lounge /> <Nav/></>} />
       </Routes>
     </Router>
   );
