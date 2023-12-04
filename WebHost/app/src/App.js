0d8cdb0b00b961386f85f7d8b17417f9795c3f9a
yeonyeonn <wuvele22@g.skku.edu>
2023-12-05 04:10:26 +0900

    add info.js & info_card.js

--- WebHost/app/src/App.js
+++ WebHost/app/src/App.js
@@ -4,14 +4,15 @@ import Intro from "./script/intro.js";
 import Lounges from "./script/lounges.js";
 import Lounge from "./script/lounge.js";
 import Nav from "./script/nav.js"
+import Info from "./script/info.js"
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Intro />} />
+        {/* <Route path="/" element={<Intro />} />
         <Route path="/lounges" element={<><Lounges /> <Nav/></>} /> 
-        <Route path="/lounge" element={<><Lounge /> <Nav/></>} />
+        <Route path="/lounge" element={<><Lounge /> <Nav/></>} /> */}
       </Routes>
     </Router>
   );
