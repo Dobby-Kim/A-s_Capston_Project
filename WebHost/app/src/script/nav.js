b0fd99319207c95670faaeab99fd70de409ca577
j2yp <jiwoo0110@gmail.com>
2023-12-05 04:17:07 +0900

    Navigation icon match URL

--- WebHost/app/src/script/nav.js
+++ WebHost/app/src/script/nav.js
@@ -3,31 +3,35 @@ import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import InfoIcon from '@mui/icons-material/Info';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { useNavigate } from 'react-router-dom';
 import "../style/nav.css"
 
 const LabelBottomNavigation = () => {
-  const [value, setValue] = React.useState('recents');
+  const navigate = useNavigate();
+
+  const [value, setValue] = React.useState('/lounges');
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
+    navigate(newValue);
   };
 
   return (
     <div className='navigation'>
         <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
-
-        <BottomNavigationAction
-            label="Spaces"
-            value="Spaces"
-            icon={<LocationOnIcon />}
-        />
-        
-        <BottomNavigationAction
-            label="Description"
-            value="Description"
-            icon={<InfoIcon />}
-        />
-        
+          {[
+            { label: 'Spaces', value: '/lounges', icon: <LocationOnIcon /> },
+            { label: 'Description', value: '/description', icon: <InfoIcon /> },
+            // Add more items as needed
+          ].map((navItem) => (
+            <BottomNavigationAction
+              key={navItem.value}
+              label={navItem.label}
+              value={navItem.value}
+              icon={navItem.icon}
+              showLabel={value === navItem.value}
+            />
+          ))}
         </BottomNavigation>
     </div>
   );
