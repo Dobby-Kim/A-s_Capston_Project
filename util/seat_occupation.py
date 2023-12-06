bc3237634bb89523a93052a2802ad0ff4cae6e42
Dobby_Kim <kimdobby@g.skku.edu>
2023-12-06 23:42:03 +0900

    chore-occupation.py: change coord correction const

--- util/seat_occupation.py
+++ util/seat_occupation.py
@@ -13,7 +13,7 @@ def init_space(n):
 def prop_to_pixel(points, width, height):
     points_prop = []
     for point in points:
-        points_prop.append([point[0]*height*0.95, point[1]*width*0.8]) # fixed
+        points_prop.append([point[0]*height, point[1]*width*0.90]) # fixed
 
     return points_prop
 
